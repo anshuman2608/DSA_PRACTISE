be0dba66688b4543f6a7741ea281f59e343d0f0f
Kumar Utkarsh HYD DIWID47 <utkarsh.kumar@zf.com>
2022-11-18 15:54:55 +0530

    Answer for all prev Sliding window questions

---
+++ Sliding_Window/min_window_sub.js
@@ -0,0 +1,25 @@
+
+// Question :--
+
+// Minimum window substring
+// This is very much interesting problem statement and lies under hard category problems. If you are able to understand and implement this problem then almost all the questions of sliding window you can easily build.
+
+// Problem statement - Given two strings as S and T. your task is to find the minimum substring length in which all the character of T occur in at least same quantity. More number of character can be there.
+
+// Example
+// First line contains first string.
+// Second line contains second string.
+// You have to return the length of minimum substring or sometime you have to also return the substring so we will see both the things in code.
+// Sample Input - 1
+
+// s = "TOTMTAPTAT"
+
+// t = "TTA"
+
+// Sample Output - 2
+
+// 3
+
+// Explanation
+
+// The minimum length substring that contains all character of pattern is TAT which is of size 3.
