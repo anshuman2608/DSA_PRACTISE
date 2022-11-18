39c0ade71c1913a6f0ee84e38a270a1bd02ff549
Kumar Utkarsh HYD DIWID47 <utkarsh.kumar@zf.com>
2022-11-18 16:09:31 +0530

    Optimised Solution for first negative in fixed window of array

--- Sliding_Window/firstNegative.js
+++ Sliding_Window/firstNegative.js
@@ -1,5 +1,5 @@
 //Print all first_Negative number in every subarray of size k from a given array.
-// Brute forse approach
+// Brute force approach
 
 
 // let arr1=[2,-3,5,-1,4,7,-9,8,6,-1,]
@@ -18,3 +18,17 @@
 // }
 // fun(arr1,10,3);
 
+
+
+// Optimised Solution:--
+
+function first_negative(arr, n, k){
+
+    for(let i = 0; i <= n - k; i++){
+        if(arr[i] < 0){
+            console.log(arr[i])
+        }
+    }
+}
+
+first_negative([2,-3,5,-1,4,7,-9,8,6,-1], 10, 4)
